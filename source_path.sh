0897335a0f95244383ba33da1b46653490ea131f
Parv-Maheshwari <parvmaheshwari2002@gmail.com>
2025-04-20 22:02:38 -0400

    init commit

---
+++ source_path.sh
@@ -0,0 +1,6 @@
+export DRLNAV_BASE_PATH=/home/parv/phd/acads/mr_project/DRL-Robot-Navigation-ROS2
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/home/parv/phd/acads/mr_project/DRL-Robot-Navigation-ROS2/src/turtlebot3_simulations/turtlebot3_gazebo/models
+export TURTLEBOT3_MODEL=waffle
+source /opt/ros/foxy/setup.bash
+source install/setup.bash
+
